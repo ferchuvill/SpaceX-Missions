8d28a986ec97680ce9f45c50378dde41de6df2b7
ferchuvill <ferchuvilloldo@gmail.com>
2022-07-26 00:29:55 -0300

    dataok

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {useState, useEffect} from 'react';
+import * as API from './services/launches';
+
+
+
+
 
 export function App() {
- return <div> Hola mundo!</div> 
+  const [launches, setLaunches] = useState([]);
+  
+    useEffect(() => {
+        API.getAllLaunches().then(setLaunches);
+        } , []);
+
+
+
+ return (
+  <>
+    <h1>SpaceX Launches</h1>
+    <ul>             
+        {launches.map(launch => (
+            <li key={launch.flight_number}> 
+                <h2>{launch.mission_name}</h2>
+                <p>{launch.details}</p>
+           </li>
+        ))}
+    </ul>
+    </>
+    );
 }
-
-
